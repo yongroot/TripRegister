4218c2d38a4b7a242f271f81505a9611e30fe7e4
yongroot <a460015041@gmail.com>
2020-07-17 00:20:03 +0800

    补充注释

--- src/main/java/com/lyg/demo/TripOS.java
+++ src/main/java/com/lyg/demo/TripOS.java
@@ -1,23 +1,21 @@
 package com.lyg.demo;
 
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * 一个利用二进制进行事件记录和判断的idea实现demo
- *
+ * <p>
  * Created by winggonLee on 2020/7/16
  */
 public class TripOS {
 
-    public static Entity create(int userSize, LocalDate minDate, LocalDate maxDate){
+    public static Entity create(int userSize, LocalDate minDate, LocalDate maxDate) {
         Entity tripOS = new Entity();
         tripOS.baseDate = minDate.toEpochDay();
-        tripOS.days = Period.between(minDate, maxDate).getDays() + 1;
+        tripOS.days = (int) (maxDate.toEpochDay() - minDate.toEpochDay()) + 1;
         tripOS.tripGroup = new HashMap<>((int) (userSize / .75f + 1));
         return tripOS;
     }
@@ -28,19 +26,35 @@ public class TripOS {
         private Map<String, int[]> tripGroup;
         private static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        public int check(String user, String date) {
-            return check(user, LocalDate.parse(date, timeFormatter));
-        }
-        public int check(String user, LocalDate date) {
-            return check(user, (int) (date.toEpochDay() - baseDate));
-        }
-
+        /**
+         * 从事件记录系统获取用户某一（天）的事件标记
+         *
+         * @param user  用户标识
+         * @param index 获取日期在日程数组的下标
+         */
         private int check(String user, int index) {
             final int[] trip = tripGroup.get(user);
             if (trip != null) {
                 return trip[safeIndex(index)];
             }
             return 0;
+        }
+
+        /**
+         * 在事件记录系统对用户某一（天）进行标记
+         *
+         * @param user       用户标识
+         * @param startIndex 标记开始下标
+         * @param endIndex   标记结束下标
+         * @param val        标记内容
+         */
+        private void mark(String user, int startIndex, int endIndex, int val) {
+            int[] ints = tripGroup.computeIfAbsent(user, k -> new int[days]);
+            startIndex = safeIndex(startIndex);
+            endIndex = safeIndex(endIndex);
+            while (startIndex <= endIndex) {
+                ints[startIndex++] = val;
+            }
         }
 
         public void mark(String user, String startDate, String endDate, int val) {
@@ -50,14 +64,15 @@ public class TripOS {
         public void mark(String user, LocalDate startDate, LocalDate endDate, int val) {
             int startIndex = (int) (startDate.toEpochDay() - baseDate);
             int endIndex = (int) (endDate.toEpochDay() - baseDate);
-            mark(user, safeIndex(startIndex), safeIndex(endIndex), val);
+            mark(user, startIndex, endIndex, val);
         }
 
-        private void mark(String user, int startIndex, int endIndex, int val) {
-            int[] ints = tripGroup.computeIfAbsent(user, k -> new int[days]);
-            while (startIndex <= endIndex) {
-                ints[startIndex++] = val;
-            }
+        public int check(String user, String date) {
+            return check(user, LocalDate.parse(date, timeFormatter));
+        }
+
+        public int check(String user, LocalDate date) {
+            return check(user, (int) (date.toEpochDay() - baseDate));
         }
 
         private int safeIndex(int index) {
